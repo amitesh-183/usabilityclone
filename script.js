9c48227d71adf7b133b91fda9eef161d248f2d39
Jameskillr <amiteshjames00718@gmail.com>
2023-02-15 21:42:54 +0530

    Responsive_mobile-update

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle_btn = document.getElementById("toggle");
+
+const nav_links = document.getElementById("nav-links");
+
+toggle_btn.addEventListener("click", () => {
+  nav_links.classList.toggle("active");
+});
